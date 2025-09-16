ba39a9da749a74d1372e8860fdb9e9bc2691efb0
Gagan Shrestha <gaganshresthacr7@gmail.com>
2025-09-16 13:59:21 +1000

    Delete lab1 directory

--- lab1/reverse.py
+++
@@ -1,8 +0,0 @@
-#to reverser the sting 
-original_string = " hello world "
-reversed_string = original_string[::-1]
-
-print("original:", original_string)
-print("reversed:", reversed_string)
-rs=original_string[0:2:3]
-print(rs)
